7993d384fa79352000e606f3595d0d5fb15c259e
Raphael Frey <159828193+RaffiFrey@users.noreply.github.com>
2026-03-13 22:01:56 +0100

    Add PostgreSQL setup with Drizzle ORM
    Includes initial database schema for "gods", migration files, `.env` example, Drizzle configuration, and necessary dependencies.

---
+++ src/db/schemas/gods.ts
@@ -0,0 +1,11 @@
+﻿import {integer, pgEnum, pgTable, varchar} from "drizzle-orm/pg-core";
+
+export const godTypeEnum = pgEnum("good_type", ["Gottheit", "Erzdämon"]);
+
+export const godsTable = pgTable("gods", {
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  aspects: varchar({ length: 255 }).array(),
+  holy_symbol: varchar({ length: 255 }),
+  secondName: varchar({ length: 255 }),
+})
