7859a80228e60f8d8561d7db2be6ee945b0a019b
Raphael Frey <159828193+RaffiFrey@users.noreply.github.com>
2026-03-13 22:24:56 +0100

    Add database seeding functionality and related configurations
    - Introduced `seed.ts` script to seed gods and demons data into the database.
    - Added `gods_demons.json` with detailed entries for gods and demons.
    - Created `tsconfig.json` with tailored TypeScript configurations.
    - Updated `package.json` to include database scripts for generation, migration, and seeding.

---
+++ src/db/seed.ts
@@ -0,0 +1,29 @@
+﻿import "dotenv/config";
+import { godsTable } from "./schemas";
+import godsData from "../../data/gods/gods_demons.json";
+import db from "./index";
+import {sql} from "drizzle-orm";
+
+async function seed() {
+
+  console.log("🌱 Seeding database");
+  console.log("🌱 Seeding gods & demons...");
+  await db.execute(sql`TRUNCATE TABLE gods RESTART IDENTITY`);
+  const values = godsData.map(entry => ({
+    name: entry.name,
+    god_type: entry.god_type as "Gottheit" | "Erzdämon",
+    aspects: entry.aspects,
+    holy_symbol: entry.holy_symbol || null,
+    secondName: entry.secondName ?? null,
+  }));
+
+  await db.insert(godsTable).values(values);
+
+  console.log(`✅ ${values.length} entries added.`);
+  process.exit(0);
+}
+
+seed().catch(error => {
+  console.error("❌ Error seeding database:", error);
+  process.exit(1);
+})
